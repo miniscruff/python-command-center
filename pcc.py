0ced9663d117b1811e1d857e15e6b86f5e4cdb43
Ronnie Smith <halfpint1170@gmail.com>
2018-04-23 21:40:31 -0700

    Fixed issues and added some simple help data for now

--- pcc.py
+++ pcc.py
@@ -1,4 +1,4 @@
-import subprocess
+from subprocess import Popen
 import sys
 import os
 import json
@@ -15,43 +15,69 @@ def load_scripts():
             return json.load(f)['scripts']
 
 
-# SCRIPTS = load_scripts()
-# settings = {}
-# # convert --arg=value as settings
-# # convert arg=value as extra scripts
-# for arg in sys.argv[1:]:
-#     split = arg.split('=')
-#     key = split[0]
-#     if key.startswith('--'):
-#         settings[key[2:]] = split[1]
-#     else:
-#         SCRIPTS[key] = split[1]
+def parse_args():
+    args_data = {}
+    extra = {}
+    for arg in sys.argv[1:]:
+        key, value = arg.split('=', maxsplit=1)
+        if key.startswith('--'):
+            args_data[key[2:]] = value
+        else:
+            extra[key] = value
+
+    args_data['extra'] = extra
+    return args_data
 
 
-def print_header(lines):
-    max_length = max(len(x) for x in lines)
-    lines = [x.ljust(max_length) for x in lines]
-    bars = '\u2550' * (max_length+2)
+def print_header(line):
+    bars = '\u2550' * (len(line)+2)
     print(''.join(['\u2554', bars, '\u2557']))
-    for line in lines:
-        print(' '.join(['\u2551', line, '\u2551']))
+    print(' '.join(['\u2551', line, '\u2551']))
     print(''.join(['\u255A', bars, '\u255D']))
 
 
-# def script_wrapper(script_key):
-#     def execute_script():
-#         script_com = SCRIPTS[script_key]
-#         print_header(['Running ' + script_key, '> ' + script_com])
-#         subprocess.Popen(script_com.split(' '))
-#     return execute_script
-# 
-# 
-# columns = int(settings['col']) if 'col' in settings else 8
-# 
-# root = Tk()
-# root.title('Python Command Center')
-# button_settings = dict(ipadx=5, ipady=5, padx=3, pady=3, sticky='EW')
-# for i, key in enumerate(SCRIPTS):
-#     button = Button(root, text=key, command=script_wrapper(key))
-#     button.grid(row=i // columns, column=i % columns, **button_settings)
-# root.mainloop()
+def script_wrapper(script_command):
+    def execute_script():
+        print_header('$ ' + script_command)
+        Popen(script_command.split(' '))
+    return execute_script
+
+
+def print_help():
+    print("""usage: pcc [options] [extra_commands]
+    Will look for pcc.json or package.json on start
+
+    options:
+      --col=N           Specifies how many columns to display
+      extra_commands:   Can specify any number of extra commands to display by
+                        using key=value pairs. Note that the key should not
+                        contain an equal sign, but the value can.
+      """)
+
+
+def main():
+    if len(sys.argv) > 1 and (sys.argv[1] == 'help' or sys.argv[1] == '-h'):
+        print_help()
+        sys.exit()
+
+    arg_data = parse_args()
+    columns = int(arg_data['col']) if 'col' in arg_data else 8
+    scripts = load_scripts()
+    if len(arg_data['extra']) > 0:
+        scripts.update(arg_data['extra'])
+
+    if scripts is None or len(scripts) == 0:
+        print_help()
+        sys.exit()
+
+    root = Tk()
+    root.title('Python Command Center')
+    button_settings = dict(ipadx=5, ipady=5, padx=3, pady=3, sticky='EW')
+    for i, key in enumerate(scripts):
+        button = Button(root, text=key, command=script_wrapper(scripts[key]))
+        button.grid(row=i // columns, column=i % columns, **button_settings)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
--- test/test_pcc.py
+++ test/test_pcc.py
@@ -30,13 +30,39 @@ def test_load_scripts_returns_data_from_package_json():
     assert scripts['demo'] == 'pytest'
 
 
+@patch('sys.argv', ['', 'custom command=python fuzzies.py'])
+def test_parse_extra_commands():
+    arg_data = pcc.parse_args()
+    assert len(arg_data['extra']) == 1
+    assert 'custom command' in arg_data['extra']
+    assert arg_data['extra']['custom command'] == 'python fuzzies.py'
+
+
+@patch('sys.argv', ['', 'command with equal=do something && x=y'])
+def test_parse_arg_with_equal():
+    arg_data = pcc.parse_args()
+    assert arg_data['extra']['command with equal'] == 'do something && x=y'
+
+
+@patch('sys.argv', ['', '--col=6'])
+def test_parse_columns():
+    arg_data = pcc.parse_args()
+    assert arg_data['col'] == '6'
+
+
 @patch('pcc.print')
 def test_print_header_contains_lines(mock_print):
-    pcc.print_header(['first line', 'second line'])
+    pcc.print_header('a simple line')
     calls = [
-        call('╔═════════════╗'),
-        call('║ first line  ║'),
-        call('║ second line ║'),
-        call('╚═════════════╝'),
+        call('╔═══════════════╗'),
+        call('║ a simple line ║'),
+        call('╚═══════════════╝'),
     ]
     assert mock_print.call_args_list == calls
+
+
+@patch('pcc.Popen')
+def test_script_wrapper_would_start_process_with_a_list(mock_popen):
+    wrapper = pcc.script_wrapper('command list')
+    wrapper()
+    mock_popen.called_with_args(['command', 'list'])
